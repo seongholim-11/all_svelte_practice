730ef877469ff7e3d67a693aae7f6b7bc5293795
seongho <damadu01@gmail.com>
2023-11-03 11:07:20 +0900

    onMount, onDestroy

---
+++ svelte-app/src/Something.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { onMount, onDestroy } from "svelte";
+
+    onMount(() => {
+        console.log("Mounted!");
+        return () => {
+            console.log('Destroy in mount')
+        }
+    });
+
+    onDestroy(()=>{
+        console.log("Destroy!");
+        
+    })
+</script>
+
+<h1>Something</h1>
