f608a97cbad71dc1dca685813cb9ef16c997cac1
seongho <damadu01@gmail.com>
2023-11-06 16:49:03 +0900

    일반 요소 바인딩(this)

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,23 +1,20 @@
-<div class="box" />
+<script>
+    import { tick } from "svelte";
+    let isShow = false;
+    let inputEl
 
-<style>
-    :global(body){
-        padding: 60px;
+    async function toggle() {
+        isShow = !isShow;
+        await tick();
+        // const inputEl = document.querySelector("input");
+        // 검색보다는 직접 바인딩하는 것이 성능에 좋음
+        inputEl && inputEl.focus();
     }
-    .box {
-        width: 100px;
-        height: 100px;
-        background-color: tomato;
-        border-radius: 10px;
-        animation: zoom .4s infinite alternate;
-    }
+</script>
 
-    @keyframes -global-zoom {
-        0% {
-            transform: scale(1);
-        }
-        100%{
-            transform: scale(1.5);
-        }
-    }
-</style>
+<button on:click={toggle}>Edit!</button>
+
+{#if isShow}
+    <!-- 요소 바인딩, 양방향 바인딩 -->
+    <input bind:this={inputEl}/>
+{/if}
