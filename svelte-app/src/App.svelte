74db5883f8ef0c9c2326fd77bfa81d987b690cc7
seongho <damadu01@gmail.com>
2023-11-06 17:24:05 +0900

    입력 요소 바인딩(Properties, group) 패턴 정리

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,20 +1,107 @@
 <script>
-    import { tick } from "svelte";
-    let isShow = false;
-    let inputEl
+    import { each } from "svelte/internal";
 
-    async function toggle() {
-        isShow = !isShow;
-        await tick();
-        // const inputEl = document.querySelector("input");
-        // 검색보다는 직접 바인딩하는 것이 성능에 좋음
-        inputEl && inputEl.focus();
-    }
+    let text = "";
+    let number = 3;
+    let checked = false;
+    let fruits = ["Apple", "Banana", "Cherry"];
+    let selectedFruits = [];
+    let group = "Banana";
+    let textarea = "123";
+    let select = "";
+    let multipleSelect = ["Banana", "Cherry"];
 </script>
 
-<button on:click={toggle}>Edit!</button>
+<!-- let text = '' -->
+<section>
+    <h2>Text</h2>
+    <input type="text" bind:value={text} />
+</section>
 
-{#if isShow}
-    <!-- 요소 바인딩, 양방향 바인딩 -->
-    <input bind:this={inputEl}/>
-{/if}
+<!-- let number = 0 -->
+<section>
+    <h2>Number/Range</h2>
+    <div>
+        <input type="number" bind:value={number} min="0" max="10" />
+    </div>
+    <div>
+        <input type="range" bind:value={number} min="0" max="10" />
+    </div>
+</section>
+
+<!-- let checked = false -->
+<section>
+    <h2>Checkbox</h2>
+    <input type="checkbox" bind:checked /> Agree?
+    <label>
+        <input type="checkbox" bind:checked /> Agree?(label wrapping)
+    </label>
+</section>
+
+<!-- let fruits = ['Apple', 'Banana', 'Cherry'] -->
+<!-- let selectedFruits = [] -->
+<section>
+    <h2>Checkbox 다중 선택</h2>
+    <strong>Selected: {selectedFruits}</strong>
+    {#each fruits as fruit}
+        <label>
+            <input type="checkbox" value={fruit} bind:group={selectedFruits} />
+            {fruit}
+        </label>
+    {/each}
+</section>
+
+<!-- let group = 'Banana' -->
+<section>
+    <h2>Radio</h2>
+    <!-- 
+    <input type="text" value="Apple" name="my radio">
+    <input type="text" value="Banana" name="my radio">
+    <input type="text" value="Cherry" name="my radio"> 
+    -->
+    <strong>Select: {group}</strong>
+    <label>
+        <input type="radio" value="Apple" bind:group /> Apple
+    </label>
+    <label>
+        <input type="radio" value="Banana" bind:group /> Banana
+    </label>
+    <label>
+        <input type="radio" value="Cherry" bind:group /> Cherry
+    </label>
+</section>
+
+<!-- let textarea = '' -->
+<section>
+    <h2>Textarea</h2>
+    <pre>{textarea}</pre>
+    <textarea bind:value={textarea} />
+</section>
+
+<!-- let select = 'Banana' -->
+<section>
+    <h2>Select 단일 선택</h2>
+    <strong>Selected: {select}</strong>
+    <div>
+        <select bind:value={select}>
+            <option disabled value="">Please select one!</option>
+            <option>Apple</option>
+            <option>Banana</option>
+            <option>Cherry</option>
+        </select>
+    </div>
+</section>
+
+<!-- let multipleSelect = ['Banana', 'Cherry'] -->
+<section>
+    <h2>Select 다중 선택(Multiple)</h2>
+    <strong>Selected: {multipleSelect}</strong>
+    <div>
+        <select multiple bind:value={multipleSelect}>
+            <option disabled value="">Please select one!</option>
+            <option>Apple</option>
+            <option>Banana</option>
+            <option>Cherry</option>
+        </select>
+    </div>
+</section>
