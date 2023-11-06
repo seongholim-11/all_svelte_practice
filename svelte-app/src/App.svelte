fdcb982410581f72bbe28d518a29d59467f0af53
seongho <damadu01@gmail.com>
2023-11-06 17:30:47 +0900

    편집 가능 요소 바인딩(contenteditable)

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,107 +1,19 @@
 <script>
-    import { each } from "svelte/internal";
-
-    let text = "";
-    let number = 3;
-    let checked = false;
-    let fruits = ["Apple", "Banana", "Cherry"];
-    let selectedFruits = [];
-    let group = "Banana";
-    let textarea = "123";
-    let select = "";
-    let multipleSelect = ["Banana", "Cherry"];
+    let innerHTML = ''
+    let textContent = 'Hello World!'
 </script>
 
-<!-- let text = '' -->
-<section>
-    <h2>Text</h2>
-    <input type="text" bind:value={text} />
-</section>
+<div contenteditable bind:innerHTML bind:textContent>
+    
+</div>
 
-<!-- let number = 0 -->
-<section>
-    <h2>Number/Range</h2>
-    <div>
-        <input type="number" bind:value={number} min="0" max="10" />
-    </div>
-    <div>
-        <input type="range" bind:value={number} min="0" max="10" />
-    </div>
-</section>
+<div>{innerHTML}</div>
+<div>{textContent}</div>
+<div>{@html innerHTML}</div>
 
-<!-- let checked = false -->
-<section>
-    <h2>Checkbox</h2>
-    <input type="checkbox" bind:checked /> Agree?
-    <label>
-        <input type="checkbox" bind:checked /> Agree?(label wrapping)
-    </label>
-</section>
-
-<!-- let fruits = ['Apple', 'Banana', 'Cherry'] -->
-<!-- let selectedFruits = [] -->
-<section>
-    <h2>Checkbox 다중 선택</h2>
-    <strong>Selected: {selectedFruits}</strong>
-    {#each fruits as fruit}
-        <label>
-            <input type="checkbox" value={fruit} bind:group={selectedFruits} />
-            {fruit}
-        </label>
-    {/each}
-</section>
-
-<!-- let group = 'Banana' -->
-<section>
-    <h2>Radio</h2>
-    <!-- 
-    <input type="text" value="Apple" name="my radio">
-    <input type="text" value="Banana" name="my radio">
-    <input type="text" value="Cherry" name="my radio"> 
-    -->
-    <strong>Select: {group}</strong>
-    <label>
-        <input type="radio" value="Apple" bind:group /> Apple
-    </label>
-    <label>
-        <input type="radio" value="Banana" bind:group /> Banana
-    </label>
-    <label>
-        <input type="radio" value="Cherry" bind:group /> Cherry
-    </label>
-</section>
-
-<!-- let textarea = '' -->
-<section>
-    <h2>Textarea</h2>
-    <pre>{textarea}</pre>
-    <textarea bind:value={textarea} />
-</section>
-
-<!-- let select = 'Banana' -->
-<section>
-    <h2>Select 단일 선택</h2>
-    <strong>Selected: {select}</strong>
-    <div>
-        <select bind:value={select}>
-            <option disabled value="">Please select one!</option>
-            <option>Apple</option>
-            <option>Banana</option>
-            <option>Cherry</option>
-        </select>
-    </div>
-</section>
-
-<!-- let multipleSelect = ['Banana', 'Cherry'] -->
-<section>
-    <h2>Select 다중 선택(Multiple)</h2>
-    <strong>Selected: {multipleSelect}</strong>
-    <div>
-        <select multiple bind:value={multipleSelect}>
-            <option disabled value="">Please select one!</option>
-            <option>Apple</option>
-            <option>Banana</option>
-            <option>Cherry</option>
-        </select>
-    </div>
-</section>
+<style>
+    div{
+        border: 1px solid black;
+        margin-bottom: 10px;
+    }
+</style>
