97a66f8e9b3293230247cc720446e68efb240b8f
seongho <damadu01@gmail.com>
2023-11-06 17:42:51 +0900

    조건 블록 패턴 정리

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,19 +1,62 @@
 <script>
-    let innerHTML = ''
-    let textContent = 'Hello World!'
+    let count = 0;
 </script>
 
-<div contenteditable bind:innerHTML bind:textContent>
-    
-</div>
+<button
+    on:click={() => {
+        count += 1;
+    }}>증가!</button
+>
+<button
+    on:click={() => {
+        count -= 1;
+    }}>감소!</button
+>
 
-<div>{innerHTML}</div>
-<div>{textContent}</div>
-<div>{@html innerHTML}</div>
+<h2>{count}</h2>
+
+<section>
+    <h2>if</h2>
+    {#if count > 3}
+        <div>count &gt; 3</div>
+    {/if}
+</section>
+
+<section>
+    <h2>if else</h2>
+    {#if count > 3}
+        <div>count &gt; 3</div>
+    {:else}
+        <div>count &lt;= 3</div>
+    {/if}
+</section>
+
+<section>
+    <h2>if else if</h2>
+    {#if count < 3}
+        <div>count &lt; 3</div>
+    {:else if count === 3}
+        <div>count === 3</div>
+    {:else}
+        <div>count &lt; 3</div>
+    {/if}
+</section>
+
+<section>
+    <h2>다중 블록</h2>
+    {#if count > 3}
+        {#if count === 5}
+            count === 5
+        {:else}
+            count &gt; 3
+        {/if}
+    {/if}
+</section>
 
 <style>
-    div{
-        border: 1px solid black;
+    section{
+        border: 1px solid orange;
         margin-bottom: 10px;
+        padding: 10px;
     }
 </style>
