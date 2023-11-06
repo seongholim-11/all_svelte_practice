8a0555600fb473d7cbaa423bfa9d7bde692cb802
seongho <damadu01@gmail.com>
2023-11-06 15:54:38 +0900

    반응성 구문 패턴 정리

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,69 @@
 <script>
-    import { tick } from "svelte";
-
     let count = 0;
-    let double = 0;
+
+    // 선언
+    $: double = count * 2;
+
+    // 블록
+    // 반응성 데이터가 있어야 함.
     $: {
-        double = count * 2;
-        console.log("double!");
+        console.log(count);
+        console.log(double);
     }
 
-    // 반응성 구문 내의 값이 변경된 후 대기 로직이 처리되고 화면이 갱신되어야
-    // 그 때 '반응성 구문'이 실행됨
-    // 즉, ssign 함수가 모두 실행되고나서 처리됨.
-    // $: double = count * 2
+    // 함수 실행
+    // count 변수가 변경되면 log()를 실행
+    $: count, log();
 
-    // count 증가 -> tick 실행 -> 화면이 갱신될 때까지 -> 
-    // 화면이 갱신되면 반응성구문 실행 -> double 증가 -> double값 출력
-    async function assign() {
+    // 즉시 실행 함수(IIFE)
+    $: count,
+        (() => {
+            console.log("iife: Heropy");
+        })();
+
+    // 조건문(if)
+    $: if(count > 0) {
+        console.log('if:', double)
+    }
+
+    // 반복문(for)
+    $: for(let i = 0; i < 3; i += 1){
+        count
+        console.log('for:', i)
+    }
+
+    // 조건문(Switch)
+    $: switch(count){
+        case 1:
+            console.log('switch: 1')
+            break
+        default:
+            console.log('switch: default')
+    }
+
+    // 유효범위
+    // 반응성 구문 내부에 반응성 변수가 어떤 방식이든 포함이 되어 있으면 실행
+    $: {
+        function scope1(){
+            console.log('scope1')
+            function scope2(){
+                console.log('scope2')
+                function scope3(){
+                    console.log('scope3', count)
+                }
+                scope3()
+            }
+            scope2()
+        }
+        scope1()
+    }
+
+    function log() {
+        console.log("fn: Heropy!");
+    }
+    function assign() {
         count += 1;
-        console.time("timer");
-        await tick();
-        console.timeEnd("timer");
-        console.log(double);
     }
 </script>
 
 <button on:click={assign}>Assign!</button>
-
-<h2>{count}</h2>
-<h2>{double}</h2>
