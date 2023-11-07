5aa6b5d4212dfb9477ff5065e7f1a6f3223742f1
seongho <damadu01@gmail.com>
2023-11-07 17:03:28 +0900

    영화 검색 API 예제 만들기

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,47 +1,46 @@
 <script>
-    let isError = false;
+    import axios from "axios";
 
-    function a() {
-        // 대기(pending): '이행'하거나 '거부'되지 않은 초기 상태.
-        return new Promise((resolve, reject) => {
-            if (isError) {
-                // 거부(rejected): 연산이 실패함.
-                reject(new Error('Sorry'));
-                return
-            }
-            setTimeout(() => {
-                console.log("a");
-                // 이행(fulfilled): 연산이 성공적으로 완료됨.
-                resolve('Heropy');
-            }, 1000);
-        });
+    let apikey = "84345988";
+    let title = "";
+    let movies = null;
+    let error = null;
+    let loading = false;
+
+    async function searchMovies() {
+        if (loading) {
+            return;
+        }
+        movies = null;
+        error = null;
+
+        loading = true;
+        try {
+            const res = await axios.get(
+                `http://www.omdbapi.com/?apikey=${apikey}&s=${title}`
+            );
+            console.log(res);
+            movies = res.data.Search;
+        } catch (err) {
+            console.log(err.message);
+            error = err;
+        } finally {
+            loading = false;
+        }
     }
-
-    // 대기(pending): '이행'하거나 '거부'되지 않은 초기 상태.
-    // 이행(fulfilled): 연산이 성공적으로 완료됨.
-    // 거부(rejected): 연산이 실패함.
-
-    // 이행(fulfilled): 연산이 성공적으로 완료됨.
-    a().then((res)=>{
-        console.log(res)
-        console.log('b')
-        // 거부(rejected): 연산이 실패함.
-    }).catch((err) => {
-        console.log(err)
-    }).finally(()=>{
-        console.log('Done!')
-    })
-
-    // async function asyncFn() {
-    //     try {
-    //         const res = await a();
-    //         console.log(res)
-    //         console.log("b");
-    //     } catch (err) {
-    //         console.log(err);
-    //     } finally{
-    //         console.log('Done!')
-    //     }
-    // }
-    // asyncFn();
 </script>
+
+<input type="text" bind:value={title} />
+<button on:click={searchMovies}>검색!</button>
+
+{#if loading}
+    <p style="color: royalblue">Loading...</p>
+{:else if movies}
+    <ul>
+        {#each movies as movie}
+            <li>{movie.Title}</li>
+        {/each}
+    </ul>
+{:else if error}
+    <p style="color: red">{error.message}</p>
+{/if}
