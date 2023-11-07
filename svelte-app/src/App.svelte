e03cea56b31db80dd6b361845dd260ca7fd4b9d2
seongho <damadu01@gmail.com>
2023-11-07 16:25:15 +0900

    비동기 처리의 이해와 사용 패턴 정리 - 2부

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,40 +1,47 @@
 <script>
+    let isError = false;
+
     function a() {
-        return new Promise((resolve) => {
+        // 대기(pending): '이행'하거나 '거부'되지 않은 초기 상태.
+        return new Promise((resolve, reject) => {
+            if (isError) {
+                // 거부(rejected): 연산이 실패함.
+                reject(new Error('Sorry'));
+                return
+            }
             setTimeout(() => {
                 console.log("a");
-                resolve();
+                // 이행(fulfilled): 연산이 성공적으로 완료됨.
+                resolve('Heropy');
             }, 1000);
         });
     }
 
-    function b() {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                console.log("b");
-                resolve();
-            }, 1000);
-        });
-    }
-    function c() {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                console.log("c");
-                resolve();
-            }, 1000);
-        });
-    }
-    function d() {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                console.log("d");
-                resolve();
-            }, 1000);
-        });
-    }
+    // 대기(pending): '이행'하거나 '거부'되지 않은 초기 상태.
+    // 이행(fulfilled): 연산이 성공적으로 완료됨.
+    // 거부(rejected): 연산이 실패함.
 
-    a()
-        .then(() => b())
-        .then(() => c())
-        .then(() => d());
+    // 이행(fulfilled): 연산이 성공적으로 완료됨.
+    a().then((res)=>{
+        console.log(res)
+        console.log('b')
+        // 거부(rejected): 연산이 실패함.
+    }).catch((err) => {
+        console.log(err)
+    }).finally(()=>{
+        console.log('Done!')
+    })
+
+    // async function asyncFn() {
+    //     try {
+    //         const res = await a();
+    //         console.log(res)
+    //         console.log("b");
+    //     } catch (err) {
+    //         console.log(err);
+    //     } finally{
+    //         console.log('Done!')
+    //     }
+    // }
+    // asyncFn();
 </script>
