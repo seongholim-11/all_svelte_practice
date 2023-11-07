acb22d5fbfb06421b10894a6dadfb612969ac582
seongho <damadu01@gmail.com>
2023-11-07 13:47:26 +0900

    키 블록

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,89 +1,16 @@
 <script>
-    let fruits = [
-        { id: 1, name: "Apple" },
-        { id: 2, name: "Banana" },
-        { id: 3, name: "Cherry" },
-        { id: 4, name: "Apple" },
-        { id: 5, name: "Orange" },
-    ];
-    let todos = [];
-    let fruits2D = [
-        [1, "Apple"],
-        [2, "Banana"],
-        [3, "Cherry"],
-        [4, "Orange"],
-    ];
+    import Count from "./Count.svelte";
 
-    let user = {
-        name: "Heropy",
-        age: 85,
-        email: "thesecon@gmail.com",
-    };
+    let reset = false;
 </script>
 
-<section>
-    <h2>기본</h2>
-    <!-- {#each 배열 as 속성} {/each} -->
-    {#each fruits as fruit}
-        <div>{fruit.name}</div>
-    {/each}
-</section>
+<!-- 연결된 데이터의 값이 변경될 때마다 내용을 파괴하고 다시 생성합니다. -->
+{#key reset}
+    <Count />
+{/key}
 
-<section>
-    <h2>순서(index)</h2>
-    <!-- {#each 배열 as 속성, 순서} {/each} -->
-    {#each fruits as fruit, index}
-        <div>{index} / {fruit.name}</div>
-    {/each}
-</section>
-
-<!-- 반복 블럭을 작성할 때는 key를 사용하는 것을 권장 -->
-<section>
-    <h2>아이템 고유화(key)</h2>
-    <!-- {#each fruits as fruit, index (fruit.id)} {/each} -->
-    {#each fruits as fruit, index (fruit.id)}
-        <div>{index} / {fruit.name}</div>
-    {/each}
-</section>
-
-<section>
-    <h2>빈 배열 처리(else)</h2>
-    <!-- {#each} {:else} {/each} -->
-    {#each todos as todo (todo.id)}
-        <div>{todo.name}</div>
-    {:else}
-        <div>아이템이 없어요!</div>
-    {/each}
-</section>
-
-<section>
-    <h2>구조 분해(destructuring)</h2>
-    <!-- {#each 배열 as {id, name}} {/each} -->
-    {#each fruits as { id, name } (id)}
-        <div>{name}</div>
-    {/each}
-</section>
-
-<section>
-    <h2>2차원 배열</h2>
-    <!-- {#each 배열 as [id, name]} {/each} -->
-    {#each fruits2D as [id, name] (id)}
-        <div>{name}</div>
-    {/each}
-</section>
-
-<section>
-    <h2>나머지 연산자(rest)</h2>
-    <!-- {#each 배열 as {id, ...rest}} {/each} -->
-    {#each fruits as { id, ...rest } (id)}
-        <div>{rest.name}</div>
-    {/each}
-</section>
-
-<section>
-    <h2>객체 데이터</h2>
-    <!-- 객체를 배열 형식으로 변환 -->
-    {#each Object.entries(user) as [key, value] (key)}
-        <div>{key}: {value}</div>
-    {/each}
-</section>
+<button
+    on:click={() => {
+        reset = !reset;
+    }}>reset</button
+>
---
+++ svelte-app/src/Count.svelte
@@ -0,0 +1,10 @@
+<script>
+    let count = 0;
+    setInterval(()=>{
+        count += 1
+    }, 1000)
+</script>
+
+<h1>
+    {count}
+</h1>
