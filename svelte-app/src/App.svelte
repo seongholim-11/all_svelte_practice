115c3863dc8b088bc6623da021e5831ce1e4b0db
seongho <damadu01@gmail.com>
2023-11-08 16:02:45 +0900

    부모에서 자식으로(Props)

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,13 +1,35 @@
 <script>
-    import { onMount } from "svelte";
-    import Heropy from "./Heropy.svelte";
+    import User from "./User.svelte";
 
-    let heropy;
-
-    onMount(() => {
-        console.log(heropy);
-        console.log(heropy.title);
-    });
+    let users = [
+        { name: "Neo", age: 85, email: "neo@abc.com" },
+        { name: "Lewis", age: 30, email: "lewis@abc.com" },
+        { name: "Evan", age: 52 },
+    ];
 </script>
 
-<Heropy title="Good~" bind:this={heropy} />
+<section>
+    {#each users as user}
+        <User name={user.name} age={user.age} email={user.email} />
+    {/each}
+</section>
+
+<section>
+    {#each users as { name, age, email }}
+        <User {name} {age} {email} />
+    {/each}
+</section>
+
+<section>
+    {#each users as user}
+        <User {...user} />
+    {/each}
+</section>
+
+<style>
+    section {
+        border: 1px solid wheat;
+        padding: 20px;
+        margin: 20px;
+    }
+</style>
