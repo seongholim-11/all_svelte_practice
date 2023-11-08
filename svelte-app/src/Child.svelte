54424902cf85ad8e018c7f2b13fa4d1d2ac5ba1f
seongho <damadu01@gmail.com>
2023-11-08 16:53:18 +0900

    기본/커스텀 이벤트 포워딩(Forwarding)

---
+++ svelte-app/src/Child.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+</script>
+
+<h2>Child!</h2>
+<button
+    on:click={() => {
+        dispatch("myEvent", {
+            myName: "Heropy!!",
+        });
+    }}
+>
+    Child click!</button
+>
