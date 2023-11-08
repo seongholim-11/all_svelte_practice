ca85be72b3182b1aaf8cb019fa1fad7457e2de49
seongho <damadu01@gmail.com>
2023-11-08 15:01:19 +0900

    다중 이벤트 핸들러

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,49 +1,20 @@
 <script>
-    let fruits = [
-        { id: 1, name: "Apple" },
-        { id: 2, name: "Banana" },
-        { id: 3, name: "Cherry" },
-    ];
+    let count = 0;
 
-    function assign(fruit) {
-        fruit.name += "!";
-        fruits = fruits;
-        // $$invalidate(0, fruits)
+    function increase() {
+        count += 1;
+    }
+    function current(e) {
+        console.log(e.currentTarget);
     }
 </script>
 
-<section>
-    {#each fruits as fruit (fruit.id)}
-        <div on:click={() => assign(fruit)}>
-            {fruit.name}
-        </div>
-    {/each}
-</section>
+<button
+    on:click={increase}
+    on:click={current}
+    on:click={() => console.log("click")}
+>
+    Click me!
+</button>
 
-<section>
-    {#each fruits as fruit (fruit.id)}
-        <!-- 반복문 내에서는 별도의 할당없이도 반응성을 가질 수 있음 -->
-        <div on:click={() => (fruit.name += "!")}>
-            {fruit.name}
-        </div>
-    {/each}
-    <!-- $$invalidate(0, each_value_1[fruit_index].name += "!", fruits) -->
-</section>
-
-<section>
-    {#each fruits as { id, name } (id)}
-        <div on:click={() => (name += "!")}>
-            {name}
-        </div>
-    {/each}
-    <!-- $$invalidate(0, each_value[each_index].name += "!", fruits) -->
-</section>
-
-<style>
-    section {
-        border: 1px solid yellowgreen;
-        margin: 10px;
-        padding: 10px;
-        font-size: 20px;
-    }
-</style>
+<h1>{count}</h1>
