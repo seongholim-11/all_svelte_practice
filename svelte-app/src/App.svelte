720cb515de02c327ef4f132a716dd24627c90222
seongho <damadu01@gmail.com>
2023-11-10 14:24:18 +0900

    커스텀 스토어 개념과 예제 1

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,8 +1,9 @@
 <script>
-    import { get } from "svelte/store";
-    import { count, double, user } from "./store";
-
-    console.log(get(count));
-    console.log(get(double));
-    console.log(get(user));
+    import { count } from "./count";
 </script>
+
+<h1>{$count}</h1>
+
+<button on:click={count.increment}>+</button>
+<button on:click={count.decrement}>-</button>
+<button on:click={count.reset}>reset</button>
--- svelte-app/src/store.js
+++
@@ -1,9 +0,0 @@
-import { writable, readable, derived } from "svelte/store";
-
-export let count = writable(1)
-export let double = derived(count, $count => $count * 2)
-export let user = readable({
-    name: 'Heropy',
-    age: 85,
-    email: 'thesecon@gmail.com'
-})
