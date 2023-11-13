1f559cee36cb0d1400059f1423588cfeba669f38
seongho <damadu01@gmail.com>
2023-11-13 15:58:24 +0900

    동적 컴포넌트 랜더링(component)

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,19 +1,31 @@
 <script>
-    import Address from "./Address.svelte";
+    import Heropy from "./Heropy.svelte";
+    import Neo from "./Neo.svelte";
 
-    let address = {
-        label: "대한민국",
-        children: [
-            {
-                label: "경기도",
-                children: [{ label: "수원" }, { label: "성남" }],
-            },
-            {
-                label: "강원도",
-                children: [{ label: "강릉" }, { label: "속초" }],
-            },
-        ],
-    };
+    let components = [
+        { name: "Heropy", comp: Heropy },
+        { name: "Neo", comp: Neo },
+    ];
+
+    let index = 1;
+    let selected = components[0].comp;
 </script>
 
-<Address {address} />
+{#each components as { name, comp }, i (name)}
+    <label>
+        <input
+            type="radio"
+            value={comp}
+            bind:group={selected}
+            on:change={() => (index = i + 1)}
+        />
+        {name}
+    </label>
+{/each}
+
+<svelte:component this={selected} {index} />
+<!-- {#if selected === "Heropy"}
+    <Heropy />
+{:else if selected === "Neo"}
+    <Neo />
+{/if} -->
