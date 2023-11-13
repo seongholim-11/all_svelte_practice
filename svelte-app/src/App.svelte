8fab0006cb4fb2cc4f4db235539ba69129a17783
seongho <damadu01@gmail.com>
2023-11-13 15:30:04 +0900

    요소 Zoom In-Out 예제

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,34 +1,32 @@
 <script>
-    let toggle = true;
-    let width = 200;
-
-    function hello(node, options = {}) {
-        const { width = "100px", height = "100px", color = "tomato" } = options;
-        console.log(node);
-        node.style.width = width;
-        node.style.height = height;
-        node.style.background = color || "tomato";
+    function zoom(node, scale = 1.5) {
+        node.style.transition = "1s";
+        function zoomIn() {
+            node.style.transform = `scale(${scale})`;
+        }
+        function zoomOut() {
+            node.style.transform = "scale(1)";
+        }
+        node.addEventListener("mouseenter", zoomIn);
+        node.addEventListener("mouseleave", zoomOut);
 
         return {
-            update: (opts) => {
-                console.log("update!", opts);
-                node.style.width = opts.width;
-            },
-            destroy: () => {
-                console.log("destroy!");
+            destroy() {
+                node.removeEventListener("mouseenter", zoomIn);
+                node.removeEventListener("mouseleave", zoomOut);
             },
         };
     }
 </script>
 
-<button on:click={() => (toggle = !toggle)}>Toggle</button>
-<button on:click={() => (width += 20)}>SizeUp</button>
+<div use:zoom />
+<div use:zoom = {0.7}></div>
 
-<div use:hello />
-{#if toggle}<div
-        use:hello={{
-            width: `${width}px`,
-            height: "70px",
-            color: "royalblue",
-        }}
-    />{/if}
+<style>
+    div {
+        width: 100px;
+        height: 100px;
+        background-color: tomato;
+        margin: 50px;
+    }
+</style>
